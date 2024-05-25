d99497dc62b120dccec22ab279dd18711b86fe4c
maruf-ahmed <marufahamed1110@gmail.com>
2024-05-25 09:48:44 +0600

    Added room delete by delete method

--- index.js
+++ index.js
@@ -108,6 +108,13 @@ async function run() {
       const result = await roomsCollection.insertOne(room);
       res.send(result);
     });
+
+    app.delete("/roomItem/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
